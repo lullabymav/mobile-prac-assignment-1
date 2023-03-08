cc6a2c2f6298f3aaefd629a22d1f15fdf45f6b59
syafira widiyanti <syafirawidiyanti11@gmail.com>
2023-03-08 13:28:21 +0700

    commit assignment 1, make login screen

---
+++ lib/HomePage().dart
@@ -0,0 +1,75 @@
+// ignore: file_names
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Screen'),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FlutterLogo(),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 40, 50, 5),
+                child: TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  autofocus: false,
+                  initialValue: '',
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                    contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    border: OutlineInputBorder(borderSide: BorderSide(), borderRadius: BorderRadius.all(Radius.circular(30)))
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 0, 50, 5),
+                child: TextFormField(
+                  keyboardType: TextInputType.visiblePassword,
+                  autofocus: false,
+                  initialValue: '',
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      hintText: 'Password',
+                      contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      border: OutlineInputBorder(borderSide: BorderSide(), borderRadius: BorderRadius.all(Radius.circular(30)))
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 15, 50, 5),
+                child: Material(
+                  shadowColor: Colors.lightBlueAccent.shade100,
+                  elevation: 3.0,
+                  child: MaterialButton(
+                    minWidth: MediaQuery.of(context).size.width,
+                    height: 40.0,
+                    onPressed: (){},
+                    child: Text('Log In'),
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                  ),
+                ),
+              ),
+            TextButton(
+                onPressed: (){},
+                child: Text('Forgot Password?',
+                style: TextStyle(
+                    color: Colors.black54)
+                ))
+            ],
+          ),
+      )
+    );
+  }
+}
+
